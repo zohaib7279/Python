356b7e8ac96eea753d03d3777b616fd38d79e710
Zohaib <zohaib72790@gmail.com>
2025-10-05 01:50:59 -0700

    create first program python

---
+++ firstprogram.py
@@ -0,0 +1,6 @@
+name = "Zohaib"
+age = 50+45-85
+price = 25.09
+print("my name is :", name)
+print("my age is :", age)
+print("my work price is :", price)
---
+++ secendprogram.py
@@ -0,0 +1,20 @@
+# Arthianl Opraters
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+# ritaional Opraters
+
+c = 5
+d = 2
+print(c == d)
+print(c != d)
+print(c >= d)
+print(c > d)
+print(c <= d)
+print(c < d)
