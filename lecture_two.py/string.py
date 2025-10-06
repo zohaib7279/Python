ad1630681d67f1de920642e9792380706a77e380
Zohaib <zohaib72790@gmail.com>
2025-10-06 04:30:29 -0700

    indexing slicing and and string function in python course lecture two

--- lecture_two.py/string.py
+++ lecture_two.py/string.py
@@ -3,4 +3,14 @@ print(len(str1))
 str2 = "World"
 print(str1 +" "+str2)
 str3 = "Zohaib abdul jabbr"
-print(str3[18])
+print(str3[8])
+str4 = "I am learn python with sharda khapara"
+print(str4.endswith("th") )
+str5 = "I am learn python with sharda khapara"
+print(str5.endswith("ra") )
+str6 = "I am learn python with sharda khapara"
+print(str6.capitalize())
+str7 = "I om learn python with shardakhapara"
+print(str7.replace("o", "a"))
+str8 = "I om learn python with shardakhapara"
+print(str8.find("n"))
