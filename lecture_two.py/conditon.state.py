d76fa56043beeb010e527deda8f75439c547afc0
Zohaib <zohaib72790@gmail.com>
2025-10-08 05:52:47 -0700

    modified the condiotn

--- lecture_two.py/conditon.state.py
+++ lecture_two.py/conditon.state.py
@@ -1,3 +1,20 @@
-age = 21
-if(age >= 18):
-    print("True")
+light = "red"
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("The light was not exist" )
+
+marks = int(input("marks of students :"))
+if(marks >= 90):
+    Grade = "A"
+elif(marks >= 80 and marks < 90):
+    Grade = "B"
+elif(marks >= 70 and marks < 80):
+    Grade = "C"
+else:
+    Grade = "D"
+print("Grade of the student ->" ,Grade)
