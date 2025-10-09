e400917a81f2067a636d587ccdaa0a20f4525981
Zohaib <zohaib72790@gmail.com>
2025-10-09 05:40:37 -0700

    list slicing positive nagetive index python

---
+++ lecture_three.py/list.py
@@ -0,0 +1,9 @@
+marks = [97 , 34 , 76 , 56 , 79 , 82]
+print(marks)
+print(len(marks))
+print(type(marks))
+student = ["Karan",45,"Arjun",56,"Zohaib",10,"Hafsa",12,"Fajar",3]
+print(student)
+price = [45,56,10,12,3]
+price[2:5]
+print(price)
