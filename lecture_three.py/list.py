7089de9cabae332efc7aa6aaff72a44e48701153
Zohaib <zohaib72790@gmail.com>
2025-10-10 05:41:20 -0700

    list tupls

--- lecture_three.py/list.py
+++ lecture_three.py/list.py
@@ -7,3 +7,21 @@ print(student)
 price = [45,56,10,12,3]
 price[2:5]
 print(price)
+list1 = [1, 2, 3]
+list1.append(4)
+print(list1)
+list2 = [1, 2, 3]
+list2.sort()
+print(list2)
+list3 = [1, 2, 3]
+list3.sort(reverse=True)
+print(list3)
+list4 = [1, 2, 3]
+list4.reverse()
+print(list4)
+list5 = [1, 2, 3]
+list5.insert(2,3)
+print(list5)
+list6= [1, 2, 3]
+list6.remove(2)
+print(list6)
