459669e02b99bcaa6bf1b4eaf7a3f3d7969bcc39
Zohaib <zohaib72790@gmail.com>
2025-10-31 03:50:09 -0700

    1 lecture

--- projects.py/to-do-app.py
+++ projects.py/to-do-app.py
@@ -10,6 +10,34 @@ def task():
     print(f"Today tasks are\n{tasks}")
 
     while True:
-        operation = int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit\Stop"))
+        operation = int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit\Stop = "))
         if operation == 1:
             add = input("Enter task you want to add = ")
+            tasks.append(add)
+            print(f"Task {add} hasbeen succesfully adding....")
+
+        elif operation == 2:
+            updated_val = input("Enter the task name you want to update = ")
+            if updated_val in tasks:
+                up = input("Enter new task = ")
+                ind = tasks.index(updated_val)
+                tasks[ind] = up
+                print(f"Updated Task {up}")
+
+        elif operation == 3:
+            del_val = input("Wich Task You Want to delete = ")
+            if del_val in tasks:
+                ind = tasks.index(del_val)
+                del tasks[ind]
+                print(f"Task {del_val} Hasbeen Deleted....")
+        
+        elif operation == 4:
+            print(f"Total tasks = {tasks}")
+        
+        elif operation == 5:
+            print("Closing the program.....")
+            break
+        else:
+            print("Invalid Input")
+
+task()
