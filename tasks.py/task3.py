25b5b51535853cc2056cdd81e319ea105465f5b9
Zohaib <zohaib72790@gmail.com>
2025-11-06 18:56:06 -0800

    phir

--- tasks.py/task3.py
+++ tasks.py/task3.py
@@ -1,11 +1,9 @@
-import sys
 first = int(input("Enter first number: "))
-def isinstance(first, str):
-    print("please Enter the valid number")
-    sys.exit()
-second = input("Enter second number: ")
-sum = first + second
-diffrence = int(first) - int(second)
-product = int(first) * int(second)
-quotient = int(first) / int(second)
-print(f"Sum = {sum} diffrence = {diffrence} product = {product} quotient = {quotient}")
+second = int(input("Enter second number: "))
+try:
+    sum_ = first + second
+    difference = first - second
+    product = first * second
+    quotient = first / second
+except ValueError:
+    print("Invalid input")
