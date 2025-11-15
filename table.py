2249fb68b5bd33ca71bf34e688568ae85a558f83
Zohaib <zohaib72790@gmail.com>
2025-11-14 19:57:40 -0800

    django frame work

--- table.py
+++ table.py
@@ -1,13 +1,13 @@
-# mul = int(input("Enter The Number = "))  
-# print(f"It is very simple lets read the table of {mul} =")                     
-# for i in range(1,11):
-#     print(mul ,"x", i, "=", mul*i)
-#     continue
-# while True:
-#     num = int(input("Enter a number: "))
+mul = int(input("Enter The Number = "))  
+print(f"It is very simple lets read the table of {mul} =")                     
+for i in range(1,11):
+    print(mul ,"x", i, "=", mul*i)
+    continue
+while True:
+    num = int(input("Enter a number: "))
 
-#     if num == 0:
-#         print("Program ended.")
-#         break
+    if num == 0:
+        print("Program ended.")
+        break
 
-#     print("You entered:", num)
+    print("You entered:", num)
