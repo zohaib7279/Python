58ded7c3dc9d33e09601c82fde1293f8e4bb1cf5
Zohaib <zohaib72790@gmail.com>
2025-11-22 15:59:31 -0800

    create system

---
+++ 01_python/createaccount.py
@@ -0,0 +1,115 @@
+print("----- CREATE ACCOUNT APP -----")
+account_list = []   # store multiple accounts
+while True:
+    print("1. Add account")
+    print("2. View account")
+    print("3. Delete account")
+    print("4. Exit")
+    try:
+
+        account = int(input("Enter the number to perform function = "))
+
+        if account == 1:
+            new_username = input("Enter your username: ")
+            while True:
+                try:
+                    if new_username == "":
+                        print("Username cannot be empty.")
+                        new_username = input("Enter your username: ")
+                    elif new_username == "1234567890":
+                        print("Username cannot be a sequence of numbers.")
+                        new_username = input("Enter your username: ")
+                    else:
+                        print("Username is valid.")
+                        break
+                except:
+                    print("An error occurred. Please try again.")
+                    new_username = input("Enter your username: ")
+
+            new_password = input("Enter your password: ")
+
+            while True:
+                try:
+                    if new_password == "":
+                        print("Password cannot be empty.")
+                        new_password = input("Enter your password: ")
+                    else:
+                        print("Password is valid.")
+                        break
+                except:
+                    print("An error occurred. Please try again.")
+                    new_password = input("Enter your password: ")
+
+            confirm_password = input("Confirm your password: ")
+
+            while True:
+                try:
+                    if confirm_password != new_password:
+                        print("Passwords do not match.")
+                        confirm_password = input("Confirm your password: ")
+                    else:
+                        print("Passwords match.")
+                        break
+                except:
+                    print("An error occurred. Please try again.")
+                    confirm_password = input("Confirm your password: ")
+
+            email = input("Enter your email: ")
+
+            while True:
+                try:
+                    if "@" not in email or "." not in email:
+                        print("Invalid email format.")
+                        email = input("Enter your email: ")
+                    else:
+                        print("Email is valid.")
+                        break
+                except:
+                    print("An error occurred. Please try again.")
+                    email = input("Enter your email: ")
+
+            phone_number = input("Enter your phone number: ")
+
+            while True:
+                try:
+                    if phone_number == "":
+                        print("Phone number cannot be empty.")
+                        phone_number = input("Enter your phone number: ")
+                    else:
+                        print("Phone number is valid.")
+                        break
+                except:
+                    print("An error occurred. Please try again.")
+                    phone_number = input("Enter your phone number: ")
+
+            # Save account
+            account_data = {
+                "username": new_username,
+                "password": new_password,
+                "email": email,
+                "phone": phone_number
+            }
+
+            account_list.append(account_data)
+            print("Account added successfully!")
+
+        elif account == 2:
+            print(account_list)
+            if len(account_list) == 0:
+                print("No add account please add account and view")
+
+        elif account == 3:
+            delete = input("Enter account number to delete = ")
+            if delete == "all":
+                del account_list
+            else:
+                pass
+
+        elif account == 4:
+            print("Closing the app.....")
+            break
+
+        else:
+            print("Invalid choice please enter the valid number in 1-4")
+    except:
+        print("please enter the valid number")
--- projects.py/rock.paper.scisser.py
+++ projects.py/rock.paper.scisser.py
@@ -1,22 +1,26 @@
 import random
-item_list = ["Rock","Paper","Scissor"]
-user_choise = input("Enter your move = Rock, Paper, Scissor = ")
-comp_choise = random.choice(item_list)
-print(f"User choice = {user_choise}, computer choice = {comp_choise}")
-if user_choise == comp_choise:
-    print("both Chooses same")
-elif user_choise == "Rock":
-    if comp_choise == "Paper":
-        print("Paper cover Rock = Computer")
-    else:
-        print("Rock smashes Scissor = You win")
-elif user_choise == "Paper":
-    if comp_choise == "Scissor":
-        print("Scissor cuts paper, Computer Win")
-    else:
-        print("Paper cover Rocks,You Win")
-elif user_choise == "Scissor":
-    if comp_choise == "Paper":
-        print("Scissor cuts paper, You Win")
-    else:
-        print("Rock smashes scissor, Computer Win")
+while True:
+    item_list = ["Rock","Paper","Scissor"]
+    user_choise = input("Enter your move = Rock, Paper, Scissor = ")
+    comp_choise = random.choice(item_list)
+    print(f"User choice = {user_choise}, computer choice = {comp_choise}")
+    if user_choise == comp_choise:
+        print("both Chooses same")
+    elif user_choise == "Rock":
+        if comp_choise == "Paper":
+            print("Paper cover Rock = Computer")
+        else:
+            print("Rock smashes Scissor = You win")
+    elif user_choise == "Paper":
+        if comp_choise == "Scissor":
+            print("Scissor cuts paper, Computer Win")
+        else:
+            print("Paper cover Rocks,You Win")
+    elif user_choise == "Scissor":
+        if comp_choise == "Paper":
+            print("Scissor cuts paper, You Win")
+        else:
+            print("Rock smashes scissor, Computer Win") 
+    elif user_choise == "exit":
+        "Closing the app..."
+        break
